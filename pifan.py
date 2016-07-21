088ef3afb9b12979438fc9d1a1143f2111242a02
Rich Larson <TheRealMrChips@users.noreply.github.com>
2016-07-21 09:09:06 -0700

    Version 2 changes - Lots of Changes...
    #Version 2.0
    
    * Replaced 'cool' mode with two new modes: cron mode and daemon mode.
    * The new 'cron' mode makes the app friendly for people who want to automate it using a cron-script.  It simply sets the fan state based on the current temperature rules as provided in the --tempOn and --tempOff values on the command line.  Then it exists.
    * The new 'daemon' mode behaves the same as the old 'cool' mode, running continuously until terminated.  This is the mode to use if you want to automate this tool using init scripts at startup, to wrap it as a service, or to simply kick it off to run in a separate shell.
    * Temperatures are now reported as float values down to the tenth of a degree.
    * Status reporting has been cleaned-up and is now consistent in format across modes.

--- pifan.py
+++ pifan.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python
 # coding: UTF-8
-#
-# pifan - Controls a fan attached to a Raspberry Pi via a GPIO pin.
-#
-# Written by Richard Larson
-#
-# Version 1.0 - 2016/07/19 - First Official Release
-#
-# Compatibility Note:
-# pifan is compatible with both Python2 and Python3, but is set to execute in Python2 because
-# it is the version installed on Raspbian by default.  To run via Python3, you will need to
-# run the following: "sudo apt-get install python3 python3-rpi.gpio" on your Pi, then update
-# the hashbang on line 1 of this file to read "#!/usr/bin/python3".
-#
-# Licensed via the MIT license: https://github.com/TheRealMrChips/pifan/blob/master/LICENSE
-# Latest version available here: https://github.com/TheRealMrChips/pifan
-#
-# For details on how to wire up a compatible cooling fan, see the README.md in the pifan GitHub repo.
-#
+
+""" 
+pifan - Controls a fan attached to a Raspberry Pi via a GPIO pin.
+
+Written by Richard Larson
+
+Version 2.0 - 2016/07/21 - Second Official Release (Many Changes from 1.0!!!)
+
+Compatibility Note:
+
+	pifan is compatible with both Python2 and Python3, but is set to execute in Python2 because
+	it is the version installed on Raspbian by default.  To run via Python3, you will need to
+	run the following: "sudo apt-get install python3 python3-rpi.gpio" on your Pi, then update
+	the hashbang on line 1 of this file to read "#!/usr/bin/python3".
+
+Licensed via the MIT license: https://github.com/TheRealMrChips/pifan/blob/master/LICENSE
+Latest version available here: https://github.com/TheRealMrChips/pifan
+Change log is available here: https://github.com/TheRealMrChrips/blob/master/CHANGELOG
+
+For details on how to wire up a compatible cooling fan, see the README.md in the pifan GitHub repo.
+"""
+
 import os
 import sys
 import time
@@ -26,138 +30,162 @@ import argparse
 import datetime
 import RPi.GPIO as GPIO
 
+# define global constants...
+_FANON = 'cooling'
+_FANOFF = 'stopped'
+_FANUNCHANGED = 'unchanged'
+_TEMPERATURECHECK = 'current-temperature'
+
+
 # parse the command-line arguments and return our default values...
 def parseargs():
 
 	parser=argparse.ArgumentParser(description='PiFan: This command allows you to control a cooling fan attached to a GPIO pin on the raspberry pi. Temperatures are in degrees celcius. GPIO pins are referenced as the physical pin numbers (1-40). In cool mode the app will emit run/stop events to STDOUT with timestamps for each to enable easy logging/monitoring.')
 
-	parser.set_defaults(action='cool')
-	parser.add_argument('-a', '--action', 
+	parser.set_defaults(mode='temp')
+	parser.add_argument('-m', '--mode', 
 		type=str,
-		choices=['run', 'stop', 'temp', 'cool'], 
-		help="Run starts the fan, stop halts it, temp displays the current temperature, cool monitors CPU temperature and turns the fan on or off according to the values of the --onTemp and --offTemp arguments. Default is Cool.")
+		choices=['run', 'stop', 'temp', 'cron', 'daemon'], 
+		help="Run mode starts the fan, stop mode halts it, temp mode just outputs the current temperature, cron mode turns the fan on or off according to the values of the --onTemp and --offTemp arguments and is designed to make fan control easy in cron scripts (hence the name). Daemon mode is the same as cron mode, but runs forever until the app is terminated. Default is 'temp' mode.")
 	
 	parser.set_defaults(pin=8)
 	parser.add_argument('-p', '--pin',
 		type=int,
 		help="Sets the GPIO board pin to use for fan control. Default is pin 8.")
 
-	parser.set_defaults(onTemp=60)
+	parser.set_defaults(onTemp=60.0)
 	parser.add_argument('-o','--onTemp',
-		type=int,
-		help="Sets the cool mode temp above which the fan will stay ON. Default is 60°C.")
+		type=float,
+		help="Sets the cron & daemon mode temperature above which the fan will be turned ON. Default is 60°C.")
 
-	parser.set_defaults(offTemp=55)
+	parser.set_defaults(offTemp=55.0)
 	parser.add_argument('-f','--offTemp',
-		type=int,
-		help="Sets the cool mode temp below which the fan will stay OFF. Default is 55°C.")
+		type=float,
+		help="Sets the cron & daemon mode temperature below which the fan will by turned OFF when it is already ON. Default is 55°C.")
 
 	parser.set_defaults(checkInterval=60)
 	parser.add_argument('-c','--checkInterval',
 		type=int,
-		help="The number of seconds pifan waits between temperature checks in cool mode. Default is 60.")
+		help="The number of seconds pifan waits between temperature checks in daemon mode. Default is 60.")
 
 	parser.set_defaults(exitAction='stop')
-	parser.add_argument('-x', '--exitAction', 
+	parser.add_argument('-x', '--daemonExitAction', 
 		type=str,
 		choices=['run', 'stop'], 
-		help="Sets the state the fan will be left in upon exit of cool mode. Default is Stop.")
+		help="Sets the state the fan will be left in upon exit of daemon mode. Default is Stop.")
 
 	parser.set_defaults(utcTime=False)
 	parser.add_argument('-u','--utcTime',
 		action="store_true",
 		help="Use UTC time on status timestamps. Default is local time.")
 
-	parser.set_defaults(verbose=False)
-	parser.add_argument('-v','--verbose',
-		action="store_true",
-		help="Tells cool mode to log status every interval regardless of fan-state changes.")
-
 	args=parser.parse_args()
 	return args
 
+
 # get integral value of current cpu temperature...
 def getCPUTempCelcius():
+
 	temp = os.popen("vcgencmd measure_temp").readline()
 	temp = temp.replace("temp=","").replace("'C\n","")
-	return int(float(temp))
+	return float(temp)
+
 
 # get current date/time as an ISO8601 formatted string...
-def getCurrentDateTimeISO8601(utc=True):
+def getCurrentDateTimeISO8601(utc=False):
+
 	if utc:
 		return datetime.datetime.utcnow().isoformat()
 	else:
 		return datetime.datetime.now().isoformat()
 
+
 # emit a status record...
-def emitStatus(fanState,currentTemp):
+def emitStatus(statusMsg,currentTemp):
+
 	printItems=[]
-	printItems.append(getCurrentDateTimeISO8601())
+	printItems.append(getCurrentDateTimeISO8601(_args.utcTime))
 	printItems.append(str(currentTemp))
-	printItems.append(fanState)
+	printItems.append(statusMsg)
 	output = '\t'.join(printItems)
 	print(output)
 	sys.stdout.flush()
 
+
 # turn the fan ON or OFF for a specified GPIO pin...
 def fanControl(pin,state):
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setwarnings(False)
-	GPIO.setup(pin, GPIO.OUT)
-	if state == _FANON:
-		GPIO.output(pin, 1)
-	elif state == _FANOFF:
-		GPIO.output(pin, 0)
 
-# cleanup routine that executes at exit of application...
+	# only take action if needed...
+	if state != _FANUNCHANGED:
+
+		# set up GPIO pin details...
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setwarnings(False)
+		GPIO.setup(pin, GPIO.OUT)
+
+		# and set high for ON, and low for OFF...
+		if state == _FANON:
+			GPIO.output(pin, 1)
+		elif state == _FANOFF:
+			GPIO.output(pin, 0)
+
+
+# cleanup routine that executes when application is terminating from daemon mode...
 def cleanupBeforeExit(signum, frame):
 
-	# always leave the fan in the desired exit mode (run or stop) and clean up GPIO pins...
+	# always leave the fan in the desired exit mode (run or stop)...
 	finalState = _FANOFF if _args.exitAction == 'stop' else _FANON
 	fanControl(_args.pin, finalState)
 	GPIO.cleanup()
 	
 	# determine reason we stopped...
 	stoppedBy = 'by-user' if signum == 0 else 'by-sigterm'
-	emitStatus('cooling-mode: ended-' + stoppedBy, getCPUTempCelcius())
-	
-	#and exit cleanly...
+	emitStatus('daemon-cooling-mode: ended-' + stoppedBy, getCPUTempCelcius())
+
+	# and exit cleanly...
 	sys.exit(0)
 
+
+# apply temperature-based rules to turn fan on or off...
+def applyCoolingRules(args):
+
+	# obtain the current CPU temperature...
+	args.currentTemp = getCPUTempCelcius()
+
+	# caclulate the action we want to take with the fan...
+	desiredFanState = _FANUNCHANGED
+	if (args.currentTemp > args.onTemp):
+		desiredFanState = _FANON
+	elif (args.currentTemp < args.offTemp):
+		desiredFanState = _FANOFF
+
+	# apply our changes...
+	fanControl(args.pin, desiredFanState)
+	args.fanState = desiredFanState
+
+	# and log our results...
+	emitStatus(args.fanState, args.currentTemp)
+
+
 # cooling routine that monitors CPU temps and automates fan control accordingly...
-def runCooling(args):
+def runDaemon(args):
 
-	# display a cool-mode status so users can log when app started/stopped...
-	emitStatus('cooling-mode: running', getCPUTempCelcius())
-
-	# set up system hooks to handle termination signals gracefully by running cleanup...
+	# set up system hook to handle termination signals gracefully by running cleanup...
 	signal.signal(signal.SIGTERM, cleanupBeforeExit)
+
+	# display a cool-mode status so users can see when app started/stopped...
+	emitStatus('daemon-cooling-mode: started', getCPUTempCelcius())
 
 	# always start with fan OFF...
 	args.fanState = _FANOFF
 	fanControl(args.pin, args.fanState)
 
-	# loop forever, testing and changing fan state as we go...
+	# daemon-loop until Ctrl-C or SIGTERM recieved...
 	try:
 		while True:
 	
-			# obtain the current CPU temperature...
-			args.currentTemp = getCPUTempCelcius()
-	
-			# caclulate the action we want to take with the fan...
-			desiredFanState = _FANUNCHANGED
-			if (args.currentTemp > args.onTemp):
-				desiredFanState = _FANON
-			elif (args.currentTemp < args.offTemp):
-				desiredFanState = _FANOFF
-
-			# only apply our action if it differs from the current state...
-			if desiredFanState != _FANUNCHANGED:	
-				fanControl(args.pin, desiredFanState)
-				args.fanState = desiredFanState
-				emitStatus(args.fanState, args.currentTemp)
-			elif args.verbose == True:
-				emitStatus(args.fanState, args.currentTemp)
+			# test and set fan state based on temperature...
+			applyCoolingRules(args)
 
 			# sleep a bit before doing it all again...
 			time.sleep(args.checkInterval)
@@ -170,29 +198,32 @@ def runCooling(args):
 # take appropriate action based on the command arguments...
 def processAction(args):
 
-	if args.action == 'run':
+	# 'run' mode - turn the fan on...
+	if args.mode == 'run':
 		fanControl(args.pin, _FANON)
 
-	elif args.action == 'stop':
+	# 'stop' mode - turn the fan off...
+	elif args.mode == 'stop':
 		fanControl(args.pin, _FANOFF)
 
-	elif args.action == 'temp':
-		currentTemp = getCPUTempCelcius()
-		print(currentTemp)
-		sys.stdout.flush()
+	# 'temp' mode - return the current temp (no fan changes in this mode)...
+	elif args.mode == 'temp':
+		emitStatus(_TEMPERATURECHECK, getCPUTempCelcius())
 
-	elif args.action == 'cool':
-		runCooling(args)
+	# 'cool' mode - adjust fan state (on/off/unchanged) based on current temperature, then exit...
+	elif args.mode == 'cron':
+		applyCoolingRules(args)
 
-#**********
-#** MAIN **
-#**********
+	# 'daemon' mode - monitor temperature and adjust fan state continuously until terminated...
+	elif args.mode == 'daemon':
+		runDaemon(args)
 
-# generate runtime variables based on the parsed user intent...
-_FANON = 'running'
-_FANOFF = 'idle'
-_FANUNCHANGED = 'unchanged'
+""" 
+**************************
+** MAIN APP STARTS HERE **
+**************************
+"""
+
+# generate runtime variables from CLI options and then execute the desired application mode...
 _args=parseargs()
-
-# and execute the given action...
 processAction(_args)
